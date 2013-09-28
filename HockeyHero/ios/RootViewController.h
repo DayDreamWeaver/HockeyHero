6654db4cfb8e6e6c5e39645490bf37d2ffb4da27
zhou han <han.zhou@gu3.co.jp>
2013-09-28 16:45:59 +0900

    start of HockeyHero

---
+++ HockeyHero/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  HockeyHeroAppController.h
+//  HockeyHero
+//
+//  Created by hanks on 2013/09/28.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
